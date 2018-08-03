3fd39aaf686ae60419699b888af0f9f3b7b6353e
Wolf Vollprecht <w.vollprecht@gmail.com>
2018-08-03 13:08:37 +0200

    propagate continuous layout through views

--- include/xtensor/xassign.hpp
+++ include/xtensor/xassign.hpp
@@ -196,11 +196,11 @@ namespace xt
                     || e2.is_trivial_broadcast(e1.strides());
         }
 
-        template <class D, class E2, class... SL>
-        inline bool is_trivial_broadcast(const xview<D, SL...>&, const E2&)
-        {
-            return false;
-        }
+        // template <class D, class E2, class... SL>
+        // inline bool is_trivial_broadcast(const xview<D, SL...>&, const E2&)
+        // {
+        //     return xview<D, SL...>::contiguous_layout;
+        // }
 
         template <class E, class = void_t<>>
         struct forbid_simd_assign
@@ -300,7 +300,8 @@ namespace xt
         const E2& de2 = e2.derived_cast();
 
         bool trivial_broadcast = trivial && detail::is_trivial_broadcast(de1, de2);
-
+        // std::cout << detail::is_trivial_broadcast(de1, de2);
+        // std::cout << "IS TRIVIAL: " << trivial_broadcast << std::endl;
         if (trivial_broadcast)
         {
             constexpr bool simd_assign = xassign_traits<E1, E2>::simd_assign();
@@ -497,8 +498,8 @@ namespace xt
         template <class E1, class E2>
         inline void trivial_assigner_run_impl(E1& e1, const E2& e2, std::true_type)
         {
-            auto src = e2.storage_cbegin();
-            auto dst = e1.storage_begin();
+            auto src = detail::trivial_begin(e2);
+            auto dst = detail::trivial_begin(e1);
             assign_loop<typename E1::value_type>(src, dst, e1.size());
         }
 
--- test/test_xview.cpp
+++ test/test_xview.cpp
@@ -22,6 +22,18 @@ namespace xt
     using std::size_t;
     using view_shape_type = dynamic_shape<size_t>;
 
+    template <class A, class B, std::ptrdiff_t BB, std::ptrdiff_t BE>
+    bool operator==(const A& lhs, const container_offset_view<B, BB, BE>& rhs)
+    {
+        return lhs.size() == rhs.size() && std::equal(rhs.begin(), rhs.end(), lhs.begin());
+    }
+
+    template <class A, class B, std::ptrdiff_t BB, std::ptrdiff_t BE>
+    bool operator==(const container_offset_view<B, BB, BE>& lhs, const A& rhs)
+    {
+        return lhs.size() == rhs.size() && std::equal(rhs.begin(), rhs.end(), lhs.begin());
+    }
+
     TEST(xview, temporary_type)
     {
         {
@@ -754,10 +766,20 @@ namespace xt
             { 5, 6 }
         };
         auto row = xt::view(a, 1, xt::all());
-        bool cond1 = std::is_same<decltype(row)::strides_type, std::array<std::ptrdiff_t, 1>>::value;
-        bool cond2 = std::is_same<decltype(row.strides()), const std::array<std::ptrdiff_t, 1>&>::value;
-        EXPECT_TRUE(cond1);
-        EXPECT_TRUE(cond2);
+        if (a.layout() == layout_type::row_major)
+        {
+            bool cond1 = std::is_same<decltype(row)::strides_type, std::array<std::ptrdiff_t, 1>>::value;
+            bool cond2 = std::is_same<decltype(row.strides()), const xt::container_offset_view<std::array<std::ptrdiff_t, 2>, 1, -1>&>::value;
+            EXPECT_TRUE(cond1);
+            EXPECT_TRUE(cond2);
+        }
+        else
+        {
+            bool cond1 = std::is_same<decltype(row)::strides_type, std::array<std::ptrdiff_t, 1>>::value;
+            bool cond2 = std::is_same<decltype(row.strides()), const std::array<std::ptrdiff_t, 1>&>::value;
+            EXPECT_TRUE(cond1);
+            EXPECT_TRUE(cond2);
+        }
     }
 
     TEST(xview, transpose)
@@ -974,9 +996,9 @@ namespace xt
         v3(0, 2, 1) = 1000;
         EXPECT_EQ(a(1, 1, 3), 1000);
 
-        bool b = detail::slices_contigous<xkeep_slice<int>, int>::value;
+        bool b = detail::is_strided_view<decltype(a), xkeep_slice<int>, int>::value;
         EXPECT_FALSE(b);
-        b = detail::slices_contigous<xrange<int>, xrange<int>, int>::value;
+        b = detail::is_strided_view<decltype(a), xrange<int>, xrange<int>, int>::value;
         EXPECT_TRUE(b);
     }
 
@@ -1033,5 +1055,75 @@ namespace xt
         EXPECT_EQ(idx2[0], exp_idx);
         exp_idx[0] = 2;
         EXPECT_EQ(idx2[1], exp_idx);
+    }
+
+    TEST(xview, contiguous)
+    {
+        using xtes = xt::xtensor<double, 4, layout_type::row_major>;
+        using xarr = xt::xarray<double, layout_type::row_major>;
+        using xfix = xt::xtensor_fixed<double, xshape<3, 4, 2, 5>, layout_type::row_major>;
+
+        using ctes = xt::xtensor<double, 4, layout_type::column_major>;
+        using carr = xt::xarray<double, layout_type::column_major>;
+        using cfix = xt::xtensor_fixed<double, xshape<3, 4, 2, 5>, layout_type::column_major>;
+
+        EXPECT_TRUE((detail::is_contiguous_view<xtes, xall<int>, xall<int>, xall<int>>()));
+        EXPECT_TRUE((detail::is_contiguous_view<xarr, xall<int>, xall<int>, xall<int>>()));
+        EXPECT_TRUE((detail::is_contiguous_view<xfix, xall<int>, xall<int>, xall<int>>()));
+        EXPECT_TRUE((detail::is_contiguous_view<xtes, int, int, xall<int>>()));
+        EXPECT_TRUE((detail::is_contiguous_view<xtes, int, xall<int>, xall<int>>()));
+        EXPECT_TRUE((detail::is_contiguous_view<xtes, int, xall<int>, xall<int>, xall<int>>()));
+        EXPECT_TRUE((detail::is_contiguous_view<xtes, int, int, xrange<int>>()));
+        EXPECT_TRUE((detail::is_contiguous_view<xtes, int, xrange<int>>()));
+        EXPECT_FALSE((detail::is_contiguous_view<xtes, int, xrange<int>, int>()));
+
+        EXPECT_TRUE((detail::is_contiguous_view<ctes, xall<int>, xall<int>, xall<int>, xall<int>>()));
+        EXPECT_FALSE((detail::is_contiguous_view<ctes, int, int, xall<int>>()));
+        EXPECT_FALSE((detail::is_contiguous_view<ctes, int, xall<int>, xall<int>>()));
+        EXPECT_FALSE((detail::is_contiguous_view<ctes, int, xall<int>, xall<int>, xall<int>>()));
+        EXPECT_FALSE((detail::is_contiguous_view<ctes, int, int, xrange<int>>()));
+        EXPECT_FALSE((detail::is_contiguous_view<ctes, int, xrange<int>>()));
+        EXPECT_FALSE((detail::is_contiguous_view<ctes, int, xrange<int>, int>()));
+
+        EXPECT_TRUE((detail::is_contiguous_view<ctes, xall<int>, xall<int>, int, int>()));
+        EXPECT_TRUE((detail::is_contiguous_view<cfix, xall<int>, xall<int>, int, int>()));
+        EXPECT_FALSE((detail::is_contiguous_view<xarr, xall<int>, xall<int>, int, int>()));
+        EXPECT_TRUE((detail::is_contiguous_view<ctes, xall<int>, xall<int>, xrange<int>, int>()));
+        EXPECT_FALSE((detail::is_contiguous_view<ctes, xall<int>, xrange<int>, xrange<int>, int>()));
+        EXPECT_FALSE((detail::is_contiguous_view<ctes, xall<int>, xrange<int>, xall<int>, int>()));
+        EXPECT_FALSE((detail::is_contiguous_view<ctes, xall<int>, xrange<int>, xrange<int>, int>()));
+        EXPECT_FALSE((detail::is_contiguous_view<ctes, xall<int>, xstepped_range<int>, int, int>()));
+    }
+
+    TEST(xview, container_offset_view)
+    {
+        auto a = std::vector<int>({1,2,3,4,5,6});
+        auto b = std::array<int, 6>({1,2,3,4,5,6});
+
+        auto va = container_offset_view<std::vector<int>, 3>(a);
+        auto vb = container_offset_view<std::array<int, 6>, 3>(b);
+
+        EXPECT_EQ(va[0], a[3]);
+        EXPECT_EQ(va[1], a[4]);
+        EXPECT_EQ(*va.end(), *a.end());
+        EXPECT_TRUE(std::equal(a.begin() + 3, a.end(), va.begin()));
+        EXPECT_EQ(a.size() - 3, va.size());
+
+        EXPECT_EQ(vb[0], b[3]);
+        EXPECT_EQ(vb[1], b[4]);
+        EXPECT_EQ(*(vb.end() - 1), *(b.end() - 1));
+        EXPECT_TRUE(std::equal(b.begin() + 3, b.end(), vb.begin()));
+        EXPECT_EQ(b.size() - 3, vb.size());
+
+        auto vae = container_offset_view<std::vector<int>, 3, 5>(a);
+        auto vbe = container_offset_view<std::array<int, 6>, 3, 5>(b);
+
+        EXPECT_EQ(vae[0], b[3]);
+        EXPECT_EQ(vae[1], b[4]);
+        EXPECT_EQ(vae.back(), b[4]);
+        EXPECT_EQ(*vae.end(), *(a.end() - 1));
+        EXPECT_TRUE(std::equal(a.begin() + 3, a.end() - 1, vae.begin()));
+        EXPECT_EQ(2, vae.size());
+
     }
 }
